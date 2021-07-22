398d5361c1140fe8afa0bf102e9f7f6b309a6e6d
dinesh0831 <81325951+dinesh0831@users.noreply.github.com>
2021-07-22 16:24:57 +0530

    Create question:4.js

---
+++ day 4/task1/question:4.js
@@ -0,0 +1,6 @@
+var firstname="dinesh";
+var lastname="palani";
+var maritalstatus="single";
+var age="24";
+var country="india";
+console.log(firstname+" "+lastname+" "+maritalstatus+" "+age+" "+country);
