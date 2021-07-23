668102cb4950347f08e178796507d3806ee44123
dinesh0831 <81325951+dinesh0831@users.noreply.github.com>
2021-07-23 10:05:21 +0530

    Update resume with json.js

--- day 4/resume with json.js
+++ day 4/resume with json.js
@@ -7,3 +7,53 @@ var resume={objective:"A highly disciplined and hardwoking individual looking fo
               declaration:"i hereby declare that information furnished above is true to the best of my knowledge"
 }
 console.log(resume)
+
+
+
+
+output:
+
+
+{objective: "A highly disciplined and hardwoking individual loo… for a responsible person to gain good experience", education: Array(3), project: Array(1), softwareskills: Array(4), personel_details: {…}, …}
+declaration: "i hereby declare that information furnished above is true to the best of my knowledge"
+education: Array(3)
+0:
+board_university: "anna university"
+institute: "university college of engineering,panruti"
+percentage: 58
+school_degree: "BE EEE"
+year: 2019
+__proto__: Object
+1:
+board_university: "stateboard"
+institute: "Adharsh vidhyalaya higher secondary school"
+percentage: 82.5
+school_degree: "HSC"
+year: 2015
+__proto__: Object
+2:
+board_university: "stateboard"
+institute: "government boys.hr.sec.school"
+percentage: 87
+school_degree: "SSLC"
+year: 2013
+__proto__: Object
+length: 3
+__proto__: Array(0)
+objective: "A highly disciplined and hardwoking individual looking for a responsible person to gain good experience"
+personel_details:
+contact_no: "1234567890"
+fathername: "palani"
+mail_ID: "dineshbabu0831@gmail.com"
+name: "dinesh"
+__proto__: Object
+project: Array(1)
+0: {title: "A hybrid technique for performance improvement in ac-dc converter system", object: "This project highlights the power qualityissues"}
+length: 1
+__proto__: Array(0)
+softwareskills: Array(4)
+0: "javascript"
+1: "puthon"
+2: "html"
+3: "css"
+length: 4
