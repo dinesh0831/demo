049d5cfba9d0146d9c48bdd30e93370b95665699
dinesh0831 <81325951+dinesh0831@users.noreply.github.com>
2021-08-03 13:25:36 +0530

    Update resume with json.js

--- day 4/resume with json.js
+++ day 4/resume with json.js
@@ -1,5 +1,5 @@
-var resume={{objective:"A highly disciplined and hardwoking individual looking for a responsible person to gain good experience"},
-              education:{
+var resume=[{objective:"A highly disciplined and hardwoking individual looking for a responsible person to gain good experience",
+              education:[
               {school_degree:"BE EEE",
                institute:"university college of engineering,panruti",
                board_university:"anna university",
@@ -14,17 +14,17 @@ var resume={{objective:"A highly disciplined and hardwoking individual looking f
                institute:"government boys.hr.sec.school",
                board_university:"stateboard",
                year:2013,
-               percentage:87}},
+               percentage:87}],
               project:
                 {title:"A hybrid technique for performance improvement in ac-dc converter system",
                  object:"This project highlights the power qualityissues"},
-              softwareskills:["javascript","puthon","html","css"],
+              softwareskills:["javascript","python","html","css"],
             personel_details:{name:"dinesh",
                               fathername:"palani",
                               contact_no:"1234567890",
                               mail_ID:"dineshbabu0831@gmail.com"},
               declaration:"i hereby declare that information furnished above is true to the best of my knowledge"
-}}
+}]
 console.log(resume)
 
 
