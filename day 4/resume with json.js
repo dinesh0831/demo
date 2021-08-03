96a0358baf7a333358731ee6eddb5855ac7b0418
dinesh0831 <81325951+dinesh0831@users.noreply.github.com>
2021-08-03 09:56:51 +0530

    Update resume with json.js

--- day 4/resume with json.js
+++ day 4/resume with json.js
@@ -1,11 +1,30 @@
-var resume={objective:"A highly disciplined and hardwoking individual looking for a responsible person to gain good experience",
-              education:[{school_degree:"BE EEE",institute:"university college of engineering,panruti",board_university:"anna university",year:2019,percentage:58},
-               {school_degree:"HSC",institute:"Adharsh vidhyalaya higher secondary school",board_university:"stateboard",year:2015,percentage:82.5},
-              {school_degree:"SSLC",institute:"government boys.hr.sec.school",board_university:"stateboard",year:2013,percentage:87}],
-              project:[{title:"A hybrid technique for performance improvement in ac-dc converter system",object:"This project highlights the power qualityissues"}],
-              softwareskills:["javascript","puthon","html","css"],personel_details:{name:"dinesh",fathername:"palani",contact_no:"1234567890",mail_ID:"dineshbabu0831@gmail.com"},
+var resume={{objective:"A highly disciplined and hardwoking individual looking for a responsible person to gain good experience"},
+              education:{
+              {school_degree:"BE EEE",
+               institute:"university college of engineering,panruti",
+               board_university:"anna university",
+               year:2019,
+               percentage:58},
+               {school_degree:"HSC",
+                institute:"Adharsh vidhyalaya higher secondary school",
+                board_university:"stateboard",
+                year:2015,
+                percentage:82.5},
+              {school_degree:"SSLC",
+               institute:"government boys.hr.sec.school",
+               board_university:"stateboard",
+               year:2013,
+               percentage:87}},
+              project:
+                {title:"A hybrid technique for performance improvement in ac-dc converter system",
+                 object:"This project highlights the power qualityissues"},
+              softwareskills:["javascript","puthon","html","css"],
+            personel_details:{name:"dinesh",
+                              fathername:"palani",
+                              contact_no:"1234567890",
+                              mail_ID:"dineshbabu0831@gmail.com"},
               declaration:"i hereby declare that information furnished above is true to the best of my knowledge"
-}
+}}
 console.log(resume)
 
 
