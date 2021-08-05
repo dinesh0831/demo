0a5978c568d0e3ada19318f4f782a99703e0c8d7
dinesh0831 <81325951+dinesh0831@users.noreply.github.com>
2021-08-05 09:58:48 +0530

    Update json with image.js

--- day 4/json with image.js
+++ day 4/json with image.js
@@ -1,8 +1,37 @@
-var technical={dimessions:{overall_length:3500,overall_length:1600,overall_height:"1490",wheelbase:"2300",track_width:{front:1405,rear:1400},
-                minimum_turning_radius:4.5,minimum_groung_clearance:170},
-                capacities:{seating_capacity:5,fueltankcapacity:"35lit"},
-                engine:{type:"KB_series",number_of_cylender:3,number_of_valved:12,capacity:"998cc/cm3",bore_stroke:"(73*79.5)mm",compression_ratio:"10:1",
-                maximum_power:"(67/6200)PS/rpm",maximum_torque:"(90/3500)Nm/rpm",fuel_distribution:"multipoint injection"},
-                transmission:{type:"5 speed MT"},chassis:{steering:"rack & pinion,power assisted",brakes:{front:"ventilates disc",rear:"drums"},
-                suspension:{front:"macpherson strut&coil apring",rear:"isolated trailinglink"},shock_absorption:"gas filled",tyre:"155/80r13"},
-                weights:{krebweight:"860-880",gross_vehicle_weight:"1320"}}
+var technical={dimessions:
+               {overall_length:3500,
+                overall_length:1600,
+                overall_height:"1490",
+                wheelbase:"2300",
+                track_width:{
+                  front:1405,
+                  rear:1400},
+                minimum_turning_radius:4.5,
+                minimum_groung_clearance:170},
+                capacities:{
+                  seating_capacity:5,
+                            fueltankcapacity:"35lit"},
+                engine:{
+                  type:"KB_series",
+                  number_of_cylender:3,
+                  number_of_valved:12,
+                  capacity:"998cc/cm3",
+                  bore_stroke:"(73*79.5)mm",
+                  compression_ratio:"10:1",
+                maximum_power:"(67/6200)PS/rpm",
+                  maximum_torque:"(90/3500)Nm/rpm",
+                  fuel_distribution:"multipoint injection"},
+                transmission:{
+                  type:"5 speed MT"},
+               chassis:{steering:"rack & pinion,power assisted",
+                        brakes:{
+                          front:"ventilates disc",
+                                rear:"drums"},
+                suspension:{
+                  front:"macpherson strut&coil apring",
+                  rear:"isolated trailinglink"},
+                        shock_absorption:"gas filled",
+                        tyre:"155/80r13"},
+                weights:{
+                  krebweight:"860-880",
+                  gross_vehicle_weight:"1320"}}
